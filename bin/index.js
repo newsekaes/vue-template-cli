cf5b3b0f458fe944542674ee6760bb758ef3634c
newsekaes <282695040@qq.com>
2021-11-08 01:55:59 +0800

    inquirer交互

--- bin/index.js
+++ bin/index.js
@@ -3,15 +3,9 @@
 /* https://blog.csdn.net/sinat_17775997/article/details/84099731 */
 
 const { Command, Argument } = require('commander');
+const inquirer = require('inquirer');
 
 const program = new Command;
-
-program
-    .command('build')
-    .description('build web site for deployment')
-    .action(() => {
-        console.log('build');
-    });
 
 program
     .command('create')
@@ -28,6 +22,41 @@ program
         console.log(`${first} + ${second} = ${first + second}`);
     })
 
-program.parse();
+program
+    .command('talk')
+    .action(async () => {
+        const { name, operator, work } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: '你好，博士，欢迎第一次来到罗德岛，请回忆您的名字：',
+                default: 'Doctor',
+            },
+            {
+                type: 'list',
+                name: 'operator',
+                message: ({ name }) => `Dr.${name}, 您希望选择下列哪位干员从事您的贴身秘书：`,
+                choices: [
+                    '阿米娅',
+                    '白金',
+                    '砾',
+                    '蓝毒',
+                    '安洁莉娜'
+                ],
+            },
+            {
+                type: 'list',
+                name: 'work',
+                message: ({ name, operator }) => `Dr.${name}, 您希望${operator}辅助您进行何种工作？`,
+                choices: [
+                    { name: '负责战斗支援和战术献策' },
+                    { name: '文件装订、打印等文书工作' },
+                    { name: '博士的伙食管理' },
+                    { name: '在博士旁边安心坐着就好', short: '吉祥物' },
+                ],
+            },
+        ]);
+        console.log(`Dr.${name}, 干员${operator}将如下辅助您：${work}`);
+    })
 
-const opts = program.opts();
+program.parse();
